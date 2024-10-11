548efad3ef97b96fbd78400d0855405ab6713dcd
Mayuri-1208 <157973514+Mayuri-1208@users.noreply.github.com>
2024-10-11 10:46:37 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class demo{
+  public static void main(String args[])
+{
+  System.out.print("This Java program is genrated on Github platform");
+  System.out.print("This Java program is Executed with Google App Engine");
+
+}
+
+}
